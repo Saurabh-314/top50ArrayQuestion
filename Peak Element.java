c4144b837f0aa238bc4aef662b5bcf054c9feb23
saurabh yadav <maxxxjob314@gmail.com>
2022-05-31 23:47:10 +0530

    initial commit

---
+++ Peak Element.java
@@ -0,0 +1,20 @@
+class Solution
+{
+	// Function to find the peak element
+	// arr[]: input array
+	// n: size of array a[]
+	public int peakElement(int[] arr,int n)
+    {
+       int l=0;
+       int h=n-1;
+       while(l<h){
+           int mid = (l+h)/2;
+           if(arr[mid] < arr[mid+1]){
+               l=mid+1;
+           }else{
+               h = mid;
+           }
+       }
+       return l;
+    }
+}
